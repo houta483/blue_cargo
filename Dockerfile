ee855ac1453c4611bdbdbf19447bd44090d30dcc
tanner-aicradle <tanner@safex.ai>
2021-01-08 08:28:14 -0800

    need to work with docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM python:3
+FROM python:3.8.2
+# FROM selenium/standalone-chrome
+
+RUN /usr/local/bin/python -m pip install --upgrade pip
 
 WORKDIR /usr/src/app
 
-COPY . /app
-COPY requirements.txt ./
-
+COPY . . 
+RUN apt-get update -y
+RUN apt-get install apt-utils -y
+RUN apt-get install build-essential libpoppler-cpp-dev pkg-config python-dev -y
 RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
-
-CMD [ "python", "/app/my_selenium.py" ]
+ENV GLUCOSE_PASSWORD French44!
+CMD [ "python", "my_selenium.py" ]
--- my_selenium.py
+++ my_selenium.py
@@ -21,11 +21,11 @@ from pdf2image import convert_from_path
 
 os.environ["GLUCOSE_PASSWORD"] = "French44!"
 final_directory = "/Users/Tanner/code/products/glucose/original_data"
-PATH = "/Users/Tanner/utils/chromedriver"
+PATH = "./utils/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 
-class Selenium_Chrome_Class():
+class Selenium_Chrome_Class:
     def __init__(self, username, password, current_url):
         self.username = username
         self.password = password
@@ -33,77 +33,80 @@ class Selenium_Chrome_Class():
         self.extracted_first_and_last_names = []
 
     def start_driver(self):
-        print('start_driver')
+        print("start_driver")
         driver.get("https://www.libreview.com/")
 
     def country_of_residence(self):
-        print('country_of_residence')
+        print("country_of_residence")
         driver.implicitly_wait(10)
-        country_of_residence_element = driver.find_element_by_id(
-            "country-select")
+        country_of_residence_element = driver.find_element_by_id("country-select")
         country_of_residence_usa = driver.find_element_by_xpath(
-            "//*[@id='country-select']/option[47]")
+            "//*[@id='country-select']/option[47]"
+        )
         country_of_residence_usa.click()
         submit_button = driver.find_element_by_id("submit-button")
         submit_button.click()
         self.current_url = driver.current_url
 
     def populate_login_elements(self):
-        print('populate_login_element')
+        print("populate_login_element")
         login_element = driver.find_element_by_id("loginForm-email-input")
-        password_element = driver.find_element_by_id(
-            "loginForm-password-input")
-        login_button_element = driver.find_element_by_id(
-            "loginForm-submit-button")
+        password_element = driver.find_element_by_id("loginForm-password-input")
+        login_button_element = driver.find_element_by_id("loginForm-submit-button")
         login_element.send_keys(self.username)
         password_element.send_keys(self.password)
         login_button_element.click()
 
     def go_to_patients_page(self):
-        print('go_to_patients_page')
+        print("go_to_patients_page")
         patients_button_element = driver.find_element_by_id(
-            "main-header-dashboard-icon")
+            "main-header-dashboard-icon"
+        )
         patients_button_element.click()
 
     def patients_table(self):
-        print('patients_table')
+        print("patients_table")
         x = 1
         time.sleep(10)
 
         parameter = driver.find_element_by_xpath(
-            f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div")
+            f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div"
+        )
 
         while parameter:
             parameter.click()
-            profile_button = driver.find_element_by_id(
-                "profile-nav-button-container")
+            profile_button = driver.find_element_by_id("profile-nav-button-container")
             profile_button.click()
-            print('profile_button')
+            print("profile_button")
             time.sleep(10)
 
             glucose_history_button = driver.find_element_by_xpath(
-                "//*[@id='reports-nav-button-container']")
+                "//*[@id='reports-nav-button-container']"
+            )
             glucose_history_button.click()
-            print('glucose_history_button')
+            print("glucose_history_button")
             time.sleep(10)
 
             try:
                 glucose_reports_button = driver.find_element_by_id(
-                    "newGlucose-glucoseReports-button")
+                    "newGlucose-glucoseReports-button"
+                )
                 glucose_reports_button.click()
-                print('glucose_repots_button')
+                print("glucose_repots_button")
                 time.sleep(10)
             except:
                 glucose_reports_button = driver.find_element_by_id(
-                    "pastGlucoseCard-report-button")
+                    "pastGlucoseCard-report-button"
+                )
                 glucose_reports_button.click()
-                print('glucose_repots_button')
+                print("glucose_repots_button")
                 time.sleep(10)
 
             download_glucose_report_button = driver.find_element_by_id(
-                "reports-print-button")
+                "reports-print-button"
+            )
             download_glucose_report_button.click()
-            print('download_glucose_report_button')
+            print("download_glucose_report_button")
             time.sleep(10)
 
             x += 1
@@ -112,12 +115,13 @@ class Selenium_Chrome_Class():
 
             try:
                 parameter = driver.find_element_by_xpath(
-                    f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div")
+                    f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div"
+                )
             except:
                 parameter = False
 
     def move_files(self):
-        print('move_files')
+        print("move_files")
         current_day = datetime.date.today()
         formatted_date = datetime.date.strftime(current_day, "%m-%d-%Y")
         starting_directory = "/Users/Tanner/Downloads"
@@ -126,13 +130,13 @@ class Selenium_Chrome_Class():
             if re.search(formatted_date, str(filename)) and filename.endswith(".pdf"):
                 file_path_to_move = os.path.join(starting_directory, filename)
                 filename = filename.replace(" ", "")
-                os.rename(file_path_to_move, f'original_data/{filename}')
+                os.rename(file_path_to_move, f"original_data/{filename}")
             else:
                 continue
 
     def create_truncated_data_files(self, metric):
         # final_directory = "/Users/Tanner/code/products/glucose/original_data"
-        print('preprocess_pdfs')
+        print("preprocess_pdfs")
         data = []
 
         for files in os.listdir(final_directory):
@@ -144,13 +148,15 @@ class Selenium_Chrome_Class():
                 with open(file_path_to_scrape, "rb") as f:
                     text_from_pdf = pdftotext.PDF(f)
 
-            pages = helper_functions.find_correct_pages(
-                "Weekly Summary", text_from_pdf)
+            pages = helper_functions.find_correct_pages("Weekly Summary", text_from_pdf)
             print(pages)
 
             helper_functions.create_truncated_data_files_helper_function(
-                pages=pages, writable_pdf=writable_pdf,
-                where_to_save_pdf="truncated_data", person=files)
+                pages=pages,
+                writable_pdf=writable_pdf,
+                where_to_save_pdf="truncated_data",
+                person=files,
+            )
 
     def write_truncated_data_files_to_extracted_data(self):
         helper_functions.write_to_extracted_data()
@@ -160,15 +166,20 @@ class Selenium_Chrome_Class():
 
     def upload_data(self):
         helper_functions.txt_to_csv()
-        google_sheets_module = google_sheet.Google_Sheets(scopes=['https://www.googleapis.com/auth/spreadsheets'],
-                                                          spreadsheet_id='1wwGhXxKS9dXEx6YM5p9qTRLtng1Rr6ASd-y07MCZaVs', sheet_range='Sheet1!A8:I1')
+        google_sheets_module = google_sheet.Google_Sheets(
+            scopes=["https://www.googleapis.com/auth/spreadsheets"],
+            spreadsheet_id="1wwGhXxKS9dXEx6YM5p9qTRLtng1Rr6ASd-y07MCZaVs",
+            sheet_range="Sheet1!A8:I1",
+        )
         google_sheets_module.main()
 
     def clean_up(self):
-        print('clean_up')
-        list_of_dirs = [glob.glob('preprocessed_data/*'),
-                        glob.glob('truncated_data/*'),
-                        glob.glob('ocr_jpg_data/*')]
+        print("clean_up")
+        list_of_dirs = [
+            glob.glob("preprocessed_data/*"),
+            glob.glob("truncated_data/*"),
+            glob.glob("ocr_jpg_data/*"),
+        ]
 
         for directory in list_of_dirs:
             for files in directory:
@@ -176,14 +187,17 @@ class Selenium_Chrome_Class():
 
 
 app = Selenium_Chrome_Class(
-    username="stevenhoughtonjr1@gmail.com", password=os.environ['GLUCOSE_PASSWORD'], current_url="https://www.libreview.com/")
+    username="stevenhoughtonjr1@gmail.com",
+    password=os.environ["GLUCOSE_PASSWORD"],
+    current_url="https://www.libreview.com/",
+)
 # app.start_driver()
 # app.country_of_residence()
 # app.populate_login_elements()
 # app.go_to_patients_page()
 # app.patients_table()
 app.move_files()
-app.create_truncated_data_files('avg')
+app.create_truncated_data_files("avg")
 app.write_truncated_data_files_to_extracted_data()
 app.filter_txt_data()
 app.upload_data()
