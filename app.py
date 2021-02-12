e97936c1f38d3d33cb86004d9fdbb047c8b8afef
tanner-aicradle <tanner@safex.ai>
2021-02-12 00:52:11 -0800

    getting to the end now, but am not able to click the button. i need to reach out to 2captcha and see what next steps are

--- app.py
+++ app.py
@@ -1,49 +1,22 @@
 import glob
 import time
+import requests
+import os
+import csv
 from helper_functions.selenium_navigation import Selenium_Chrome_Class
 from helper_functions.google_sheet import Google_Sheets
 from googleapiclient.discovery import build
 from helper_functions import google_sheet
-import os
 from selenium import webdriver
-import csv
 from helper_functions import selenium_helper
 
 
-class Glucose_Slim:
-    def upload_data(self):
-        google_sheets_module = google_sheet.Google_Sheets(
-            scopes=["https://www.googleapis.com/auth/spreadsheets"],
-        )
-
-        google_sheets_module.get_credentials()
-
-        service = build("sheets", "v4", credentials=google_sheets_module.credentials)
-
-        with open("csvfile.csv") as csv_data_file:
-            csv_data = []
-            csv_reader = csv.reader(csv_data_file)
-
-            for line in csv_reader:
-                csv_data.append(line)
-
-            content = csv_data
-
-        resource = {"majorDimension": "ROWS", "values": content}
-
-        service.spreadsheets().values().append(
-            spreadsheetId=os.environ["SPREADSHEET_ID"],
-            range="Table_Metrics!A:A",
-            body=resource,
-            valueInputOption="USER_ENTERED",
-        ).execute()
-
-    def clean_up(self):
-        print("final clean_up")
-        os.remove("csvfile.csv")
+# class Glucose_Captcha:
+# print("here")
 
 
-app = Glucose_Slim()
+# app = Glucose_Captcha()
+
 selenium_navigator = Selenium_Chrome_Class(
     username=os.environ["USERNAME"],
     password=os.environ["GLUCOSE_PASSWORD"],
@@ -55,10 +28,4 @@ if __name__ == "__main__":
     selenium_navigator.country_of_residence()
     selenium_navigator.populate_login_elements()
     selenium_navigator.go_to_patients_page()
-    selenium_navigator.get_all_data_from_patients_table()
-
-    app.upload_data()
-    app.clean_up()
-
-    print("sleeping for two min to get the timing right")
-    time.sleep(720)
+    selenium_navigator.click_on_patients_cell_in_table()
--- helper_functions/selenium_navigation.py
+++ helper_functions/selenium_navigation.py
@@ -1,14 +1,15 @@
 import re
 import glob
 import io
-from selenium import webdriver
 import os
 import time
 import datetime
 import pytz
 import shutil
-from helper_functions import google_sheet
 import sys
+import requests
+from helper_functions import google_sheet
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from helper_functions import selenium_helper
@@ -84,67 +85,153 @@ class Selenium_Chrome_Class:
         )
         patients_button_element.click()
 
-    def get_all_data_from_patients_table(self):
+    def go_to_patient_page(self, patients_cell):
+        patients_cell.click()
+
+    def go_to_profile_of_patient(self, driver):
+        profile_button = driver.find_element_by_id("profile-nav-button-container")
+        profile_button.click()
+
+    def click_on_hyperlink_to_get_glucose_data(self, driver):
+        download_glucose_data_hyperlink = driver.find_element_by_xpath(
+            '//*[@id="patient-profile-data-download-button"]'
+        )
+        download_glucose_data_hyperlink.click()
+
+    def return_to_dashboard(self, driver):
+        driver.get("https://www.libreview.com/dashboard")
+
+    def increment_patients_cell(self, driver, x):
+        try:
+            patients_cell = driver.find_element_by_xpath(
+                f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div"
+            )
+        except:
+            patients_cell = False
+
+        return patients_cell
+
+    def get_k_parameter_re_captcha_v2(self, driver) -> str:
+        iframe_with_k_parameter = driver.find_element_by_xpath(
+            '//*[@id="exportData-reCAPTCHA"]/div/div/div/div/iframe'
+        )
+        src_attribute = iframe_with_k_parameter.get_attribute("src")
+        regex_pattern = "((k=).+?(?=&))"
+        k_parameter = re.search(regex_pattern, src_attribute).group()[2:]
+
+        return k_parameter
+
+    def re_captcha_v2_post(self, driver) -> str:
+        print("re_captcha_v2_post")
+        post_api_endpoint = "http://2captcha.com/in.php"
+
+        api_key = os.getenv("CAPTCHA_API_KEY")
+        post_method = "userrecaptcha"
+        post_google_key = self.get_k_parameter_re_captcha_v2(driver=driver)
+        post_page_url = driver.current_url
+
+        post_api_params = {
+            "key": api_key,
+            "method": post_method,
+            "googlekey": post_google_key,
+            "pageurl": post_page_url,
+        }
+
+        id_re_captcha_v2 = requests.post(
+            url=post_api_endpoint, params=post_api_params, json=1
+        ).text[3:]
+        time.sleep(20)
+
+        return id_re_captcha_v2
+
+    def g_recaptcha_response(self, driver, answer_token) -> None:
+        javascript_make_textarea_visible_string = (
+            f"document.getElementById('g-recaptcha-response').style.display = 'inline'"
+        )
+        driver.execute_script(javascript_make_textarea_visible_string)
+        time.sleep(3)
+
+        javascript_add_innerHTML_string = f"document.getElementById('g-recaptcha-response').innerHTML='{answer_token}';"
+        driver.execute_script(javascript_add_innerHTML_string)
+        time.sleep(3)
+
+        javascript_make_button_visible_string = f"document.getElementById('exportData-modal-download-button').disabled = false;"
+        driver.execute_script(javascript_make_button_visible_string)
+        time.sleep(3)
+
+    def submit_finished_captcha(self, driver):
+        print("submit_finished_captcha")
+        submit_captcha_button = driver.find_element_by_xpath(
+            '//*[@id="exportData-modal-download-button"]'
+        )
+        submit_captcha_button.click()
+
+    def re_captcha_v2_get(self, driver) -> None:
+        print("re_captcha_v2_get")
+        get_api_endpoint = "http://2captcha.com/res.php"
+
+        api_key = os.getenv("CAPTCHA_API_KEY")
+        get_action = "get"
+        get_id = self.re_captcha_v2_post(driver=driver)
+
+        get_api_params = {"key": api_key, "action": get_action, "id": get_id}
+
+        answer_token = requests.get(url=get_api_endpoint, params=get_api_params).text
+
+        recount = 1
+        wait_time = 10
+
+        while answer_token == "CAPCHA_NOT_READY":
+            print(
+                f"CAPCHA_NOT_READY for the {recount} time and will wait {wait_time} seconds now"
+            )
+
+            time.sleep(wait_time)
+
+            recount += 1
+            wait_time += 10
+
+            answer_token = requests.get(
+                url=get_api_endpoint, params=get_api_params
+            ).text
+
+        print("answer token received")
+        trimmed_answer_token = answer_token[3:]
+        self.g_recaptcha_response(driver=driver, answer_token=trimmed_answer_token)
+        time.sleep(5)
+
+        self.submit_finished_captcha(driver=driver)
+
+    def click_on_patients_cell_in_table(self) -> None:
         print("patients_table")
-        now_time = datetime.datetime.now(pytz.timezone("America/Chicago"))
-        central_time_zone = now_time.strftime("%m/%d/%y %H:%M:%S")
-
-        print(central_time_zone)
-
-        row_index = 1
-        cell_index = 1
+        x = 1
 
         print("sleep for 10")
         time.sleep(10)
 
-        row = driver.find_element_by_xpath(
-            f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{row_index}]"
+        patients_cell = driver.find_element_by_xpath(
+            f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{x}]/td[1]/div"
         )
 
-        cell = driver.find_element_by_xpath(
-            f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{row_index}]/td[{cell_index}]"
-        )
+        while patients_cell:
+            self.go_to_patient_page(patients_cell=patients_cell)
+            time.sleep(5)
 
-        while row:
-            first_cell = True
-            while cell:
-                if first_cell == True:
-                    with open("csvfile.csv", "a") as file:
-                        file.write(central_time_zone + ",")
+            self.go_to_profile_of_patient(driver=driver)
+            time.sleep(5)
 
-                    first_cell = False
+            self.click_on_hyperlink_to_get_glucose_data(driver=driver)
+            time.sleep(5)
 
-                else:
-                    with open("csvfile.csv", "a") as file:
-                        file.write(str(cell.text + ","))
+            self.get_k_parameter_re_captcha_v2(driver=driver)
+            time.sleep(5)
 
-                    cell_index += 1
+            id_of_captcha = self.re_captcha_v2_get(driver=driver)
 
-                try:
-                    cell = driver.find_element_by_xpath(
-                        f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{row_index}]/td[{cell_index}]"
-                    )
-                except:
-                    with open("csvfile.csv", "a") as file:
-                        file.write("\n")
-                    cell = False
+            x += 1
 
-            else:
-                cell_index = 1
-                row_index += 1
+            self.return_to_dashboard(driver=driver)
+            time.sleep(5)
 
-                try:
-                    row = driver.find_element_by_xpath(
-                        f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{row_index}]"
-                    )
-
-                    cell = driver.find_element_by_xpath(
-                        f"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/table/tbody/tr[{row_index}]/td[{cell_index}]"
-                    )
-                except:
-                    with open("csvfile.csv", "a") as file:
-                        file.write(
-                            "-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-"
-                        )
-
-                    row = False
+            patients_cell = self.increment_patients_cell(driver=driver, x=x)
+            time.sleep(5)
