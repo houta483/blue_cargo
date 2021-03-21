0c1a2342b664056ed5f17a43e9f9b8ace6c82b5d
tanner-aicradle <tanner@safex.ai>
2021-03-21 00:50:49 -0700

    init

--- clean_data.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ./truncated_data/*
-rm -rf ./preprocessed_data/*
-rm -rf ./extracted_and_filtered_data/*
-rm -rf ./final_csv_data/*
-rm -rf ./extracted_data/*
--- push_ecr.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 286380851113.dkr.ecr.us-west-2.amazonaws.com
-docker build -t houta483/glucose .
-docker tag houta483/glucose:latest 286380851113.dkr.ecr.us-west-2.amazonaws.com/houta483/glucose:latest
-docker push 286380851113.dkr.ecr.us-west-2.amazonaws.com/houta483/glucose:latest
